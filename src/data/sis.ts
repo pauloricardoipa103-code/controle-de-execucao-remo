17c429b76421e030474789de270190f8e2f86e45
pauloricardoipa103-code <pauloricardoipa103@gmail.com>
2026-04-24 14:41:25 -0300

    Source Deploy: src/data/sis.ts

---
+++ src/data/sis.ts
@@ -0,0 +1,88 @@
+import type { SIResponse } from '../types';
+
+export const PRELOADED_SIS: SIResponse[] = [
+  {
+    si: "100234",
+    os: "2024-001",
+    poste: "P-1002",
+    tipo_servico: "Troca de Isolador de Ancoragem",
+    localidade: "Iporá - GO (Centro)",
+    equipe: "IPOV-023M"
+  },
+  {
+    si: "100235",
+    os: "2024-002",
+    poste: "P-1008",
+    tipo_servico: "Manutenção Preventiva de Chave Fusível",
+    localidade: "Iporá - GO (Zona Rural)",
+    equipe: "IPOV-023M"
+  },
+  {
+    si: "200451",
+    os: "2024-015",
+    poste: "P-2041",
+    tipo_servico: "Substituição de Cruzeta",
+    localidade: "Iporá - GO (Jardim Novo)",
+    equipe: "IPOV-025M"
+  },
+  {
+    si: "300678",
+    os: "2024-032",
+    poste: "P-981",
+    tipo_servico: "Instalação de Chave Faca",
+    localidade: "Iporá - GO (Distrito Industrial)",
+    equipe: "IPOV-022M"
+  },
+  {
+    si: "400112",
+    os: "2024-041",
+    poste: "P-321",
+    tipo_servico: "Apoio - Limpeza de Faixa / Poda",
+    localidade: "Iporá - GO",
+    equipe: "IPOO-022M"
+  },
+  {
+    si: "400119",
+    os: "2024-048",
+    poste: "P-455",
+    tipo_servico: "Apoio Linha Viva - Transporte Equipamento",
+    localidade: "Iporá - GO",
+    equipe: "IPOO-024M"
+  },
+  {
+    si: "1018843",
+    os: "1401010",
+    poste: "27749368",
+    tipo_servico: "POSTE - DESLOCAMENTO/SUBSTITUIÇÃO (PTP)",
+    localidade: "ISRAELANDIA - GO (ZONA RURAL - ESTRADA RURAL)",
+    equipe: "IPOV-025M",
+    setor_solicitante: "REGIONAL IPORA - MANUTENCAO",
+    natureza: "PROGRAMADA",
+    inicio_previsto: "25/04/2026 07:30",
+    termino_previsto: "26/04/2026 18:00:00",
+    tipo_projeto: "Manutencao",
+    status_ptp: "Confirmado"
+  },
+  {
+    si: "1018920",
+    os: "1400960",
+    poste: "20629522",
+    tipo_servico: "OBRA REDE ZONA LIVRE-IMPLANTAÇÃO POSTE",
+    localidade: "ARENOPOLIS - GO (ZONA RURAL - ESTRADA RURAL)",
+    equipe: "IPOO-OBRAS",
+    setor_solicitante: "REGIONAL IPORA - OBRAS",
+    natureza: "PROGRAMADA",
+    inicio_previsto: "25/04/2026 07:30",
+    termino_previsto: "26/04/2026 18:00:00",
+    tipo_projeto: "Obras",
+    status_ptp: "Confirmado"
+  },
+  {
+    si: "1018843",
+    os: "1401010",
+    poste: "27749368",
+    tipo_servico: "APOIO - POSTE - DESLOCAMENTO/SUBSTITUIÇÃO",
+    localidade: "ISRAELANDIA - GO (ZONA RURAL - ESTRADA RURAL)",
+    equipe: "IPOO-022M"
+  }
+];
