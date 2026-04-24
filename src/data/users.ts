827e22ed589924a7f27c2e695602ec9d8603fe05
pauloricardoipa103-code <pauloricardoipa103@gmail.com>
2026-04-24 14:41:26 -0300

    Source Deploy: src/data/users.ts

---
+++ src/data/users.ts
@@ -0,0 +1,50 @@
+export const EQUIPES = [
+  {
+    codigo: "73304",
+    tipo: "LV PESADA",
+    base: "IPOV-023M",
+    usuarios: [
+      { nome: "LUCAS HENRIQUE SILVA LOPES", funcao: "ENCARREGADO" }
+    ]
+  },
+  {
+    codigo: "732003",
+    tipo: "LV PESADA",
+    base: "IPOV-025M",
+    usuarios: [
+      { nome: "RICARDO TOMAS DE AQUINO", funcao: "ENCARREGADO" }
+    ]
+  },
+  {
+    codigo: "73304",
+    tipo: "LV PESADA",
+    base: "IPOV-022M",
+    usuarios: [
+      { nome: "RAFAEL SANTOS PEREIRA", funcao: "ENCARREGADO" }
+    ]
+  },
+  {
+    codigo: "873004",
+    tipo: "APOIO - LINHA ML",
+    base: "IPOO-022M",
+    usuarios: [
+      { nome: "BRUNO BARBOSA DOS REIS", funcao: "RESPONSÁVEL" }
+    ]
+  },
+  {
+    codigo: "873008",
+    tipo: "APOIO - LM",
+    base: "IPOO-024M",
+    usuarios: [
+      { nome: "VINICIUS CAITANO DE OLIVEIRA", funcao: "RESPONSÁVEL" }
+    ]
+  },
+  {
+    codigo: "OBRAS-01",
+    tipo: "OBRAS E MELHORIAS",
+    base: "IPOO-OBRAS",
+    usuarios: [
+      { nome: "CARLOS AFONSO DA SILVA", funcao: "ENCARREGADO" }
+    ]
+  }
+];
